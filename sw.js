42fa01d76daae92ec7e67c9cc5259ef323bf8909
orionvale19 <orionvale19@gmail.com>
2026-05-29 04:26:15 +0800

    Refine mobile login layout

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "emby-music-web-v0.92.86";
-const ASSET_VERSION = "0.92.86";
+const CACHE_NAME = "emby-music-web-v0.92.87";
+const ASSET_VERSION = "0.92.87";
 const versioned = (path) => `${path}?v=${ASSET_VERSION}`;
 const APP_SHELL = [
   "./",
