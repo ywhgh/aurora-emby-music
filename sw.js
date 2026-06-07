7aaa0be9648c52b04237c8c1b833412f32987491
orionvale19 <orionvale19@gmail.com>
2026-06-08 01:46:19 +0800

    Release v0.93.121

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "emby-music-web-v0.93.120";
-const ASSET_VERSION = "0.93.120";
+const CACHE_NAME = "emby-music-web-v0.93.121";
+const ASSET_VERSION = "0.93.121";
 const versioned = (path) => `${path}?v=${ASSET_VERSION}`;
 const APP_SHELL = [
   "./",
