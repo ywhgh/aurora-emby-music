1eb30a32a973b600a59700ec2bc723baab65138b
orionvale19 <orionvale19@gmail.com>
2026-07-07 02:44:29 +0800

    Release v0.93.216 mobile UI refinements

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "emby-music-web-v0.93.215";
-const ASSET_VERSION = "0.93.215";
+const CACHE_NAME = "emby-music-web-v0.93.216";
+const ASSET_VERSION = "0.93.216";
 const versioned = (path) => `${path}?v=${ASSET_VERSION}`;
 const APP_SHELL = [
   "./",
