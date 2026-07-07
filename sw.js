ef752a442b1a9c41f0188b9728996bbcb572c734
orionvale19 <orionvale19@gmail.com>
2026-07-07 09:35:51 +0800

    Release v0.93.227 rebuild mobile minimal UI shell

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "emby-music-web-v0.93.226";
-const ASSET_VERSION = "0.93.226";
+const CACHE_NAME = "emby-music-web-v0.93.227";
+const ASSET_VERSION = "0.93.227";
 const versioned = (path) => `${path}?v=${ASSET_VERSION}`;
 const APP_SHELL = [
   "./",
